e9c36e4893be0bb68a5e35dc755e1b1a5c26da57
raspberry <54708046+sysdl132@users.noreply.github.com>
2021-07-05 16:38:47 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
-echo bootstrap
+# echo bootstrap
 
-VERSION="5.0-0"; \
-  time /bin/bash bootstrap/bootstrap.sh \
-    --use-cache \
-    --arch amd64 \
-    --distribution univention-corporate-server \
-    --codename ucs$(echo ${VERSION} | tr --complement --delete '[:digit:]')
+# VERSION="5.0-0"; \
+#   time sudo /bin/bash bootstrap/bootstrap.sh \
+#     --use-cache \
+#     --arch amd64 \
+#     --distribution univention-corporate-server \
+#     --codename ucs$(echo ${VERSION} | tr --complement --delete '[:digit:]')
 
 echo docker
 
-VERSION="5.0-0"; IMAGE="univention-corporate-server-debootstrap"; TAG="latest"; \
+VERSION="5.0-0"; IMAGE="univention/univention-corporate-server-debootstrap"; TAG="latest"; \
   time docker build \
     --build-arg DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
     --build-arg VERSION=${VERSION} \
